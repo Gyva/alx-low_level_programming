06d5a03af00128571f421cb7f52cdb09c4fbc937
Christian <umwerechr97@gmail.com>
2023-08-10 14:15:41 -0700

    add 101-mul.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,22 +1,24 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stddef.h>
 
 /**
- * _realloc - Reallocates a memory block using malloc and free
- * @ptr: Pointer to the memory block previously allocated with malloc
+ * _realloc - Reallocate a memory block using malloc and free
+ * @ptr: Pointer to the memory previously allocated with malloc
  * @old_size: Size of the allocated space for ptr
  * @new_size: New size of the memory block
  *
- * Return: Pointer to the reallocated memory block
+ * Return: Pointer to the newly reallocated memory block
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
     void *new_ptr;
+    char *old_ptr = ptr;
+    char *new_ptr_casted;
 
     if (new_size == old_size)
         return ptr;
 
-    if (!ptr)
+    if (ptr == NULL)
         return malloc(new_size);
 
     if (new_size == 0)
@@ -26,14 +28,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
     }
 
     new_ptr = malloc(new_size);
-
-    if (!new_ptr)
+    if (new_ptr == NULL)
         return NULL;
 
+    new_ptr_casted = new_ptr;
     for (unsigned int i = 0; i < old_size && i < new_size; i++)
-        *((char *)new_ptr + i) = *((char *)ptr + i);
+        new_ptr_casted[i] = old_ptr[i];
 
     free(ptr);
     return new_ptr;
 }
-
