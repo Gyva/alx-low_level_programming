90f31bcccf040cc80ed5e8848b9286c63db661da
Christian <umwerechr97@gmail.com>
2023-08-16 15:48:06 -0700

    fix run time error

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,10 +1,15 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef CALC_H
+#define CALC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct op - Struct op
- * @op:  operator
- * @f:function to be performed 
+ *
+ * @op: The operator
+ * @f: The function associated
  */
 typedef struct op
 {
