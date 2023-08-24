1717d9364f2ef98ba84a31b859a2b4714132a50e
Christian <umwerechr97@gmail.com>
2023-08-24 16:16:08 -0700

    correct runtime error

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,41 +1,25 @@
-#include <lists.h>
-#include <stdio.h>
-#include <stddef.h> /* For size_t */
-
+#include "lists.h"
 /**
- * struct list_t - A linked list structure
- * @str: String pointer
- * @len: Length of the string
- * @next: Pointer to the next node in the list
- */
-typedef struct list_t {
-    char *str;
-    size_t len;
-    struct list_t *next;
-} list_t;
-
-/**
- * print_list - Print elements of a linked list
+ * print_list -  Print elements of a linked list
  * @h: Pointer to the head of the linked list
- * 
  * Return: Number of nodes in the list
  */
 size_t print_list(const list_t *h)
 {
-    size_t len = 0; /* Length of the list */
+	size_t i = 0;
 
-    while (h)
-    {
-        if (!(h->str))
-        {
-            printf("[0] (nil)\n");
-        }
-        else
-        {
-            printf("[%lu] %s\n", h->len, h->str); /* Print length and string */
-        }
-        h = h->next;
-        len++;
-    }
-    return len;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		i++;
+		h = h->next;
+	}
+	return (i);
 }
