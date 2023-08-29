4178f317b96729316f63775c0767235b10732d3c
Christian <umwerechr97@gmail.com>
2023-08-29 15:06:10 -0700

    Fix betty documentation and file readablity

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,22 +4,22 @@
 /**
 * add_nodeint - adds a new node at the beginning of a singly linked list.
 * @head: a linked list to print
-* @j: value to be added to the node
+* @n: value to be added to the node
 *
 * Return: the address of the new element, or NULL if it failed.
 */
 
-listint_t *add_nodeint(listint_t **head, const int j)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_node;
+	listint_t *new_node; /* Create a new node */
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->j = j;
-	new_node->next = (*head);
-	(*head) = new_node;
+	new_node->n = n;
+	new_node->next = (*head); /* Point it to old head */
+	(*head) = new_node;	/* Point head to new node */
 
 	return (*head);
 }
