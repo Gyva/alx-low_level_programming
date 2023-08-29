261f37e9834b644b232bdf502bc695c45b25be53
Christian <umwerechr97@gmail.com>
2023-08-29 15:21:44 -0700

    Add 6-pop_listint.c file

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* pop_listint - adds a new node at the beginning of a singly linked list.
+* @head: a linked list to print
+*
+* Return: the address of the new element, or NULL if it failed.
+*/
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int value = 0;
+
+	if (*head == NULL)
+		return (0);
+
+	tmp_node = *head;
+	*head = tmp->next;
+	value = tmp->n;
+	free(tmp);
+
+	return (value);
+}
