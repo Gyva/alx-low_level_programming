d02c627cba2110424fb9984b30a8a6ccca2b499a
Christian <umwerechr97@gmail.com>
2023-08-29 14:57:48 -0700

    add 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -4,20 +4,20 @@
 
 /**
 * listint_len - prints all the elements of a linked list.
-* @n: a linked list to print
+* @h: a linked list to print
 *
 * Return: the number of nodes
 */
 
-size_t listint_len(const listint_t *n)
+size_t listint_len(const listint_t *h)
 {
-        size_t counter = 0;
+	size_t counter = 0;
 
-        while (n != NULL)
-        {
-                ++counter;
-                n = n->next;
-        }
+	while (h != NULL)
+	{
+		++counter;
+		h = h->next;
+	}
 
 return (counter);
 }
