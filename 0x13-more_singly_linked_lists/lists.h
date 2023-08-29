f9762a7feb75ecaf3cc8539fe8e06a723a4f8870
Christian <umwerechr97@gmail.com>
2023-08-29 15:52:21 -0700

    Add Update lists.h file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -36,7 +36,7 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-size_t check_looped_listint(const listint_t *head);
-size_t check_looped_listfree(listint_t *head);
+size_t check_listint(const listint_t *head);
+size_t check_listfree(listint_t *head);
 
 #endif
